5a39b8204b9c9827d1fc1e01b5d79987f3b08c50
Javier Luque <javier.lr9@gmail.com>
2024-03-07 22:29:13 +0100

    added gh-page base config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio',
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
